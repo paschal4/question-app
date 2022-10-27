2c426357c2c84f31d2069d37739d8a23370e472a
paschal23 <okongwupaschal739@gmail.com>
2022-10-27 07:47:23 -0600

    End of question app

---
+++ index.js
@@ -0,0 +1,12 @@
+let  Name= prompt(' Please enter your Name');
+let   occupa= prompt( Name + "," +'What do you do for a living' )
+let Age= prompt("Enter your age")
+let gender= prompt(' Please enter your gender');
+let  robot= prompt(' are you a robot');
+let message1 = "your name is "
+let message4 =" you are a "+occupa
+let message5 = " , your are  also a "+gender
+let message2 =" And you are a "
+let message3=" years old "
+let  log = message1+Name+message4+ message5+message2+Age+message3
+alert(log)
